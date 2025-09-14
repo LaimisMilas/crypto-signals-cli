273c2a26f18bf1d9619086b1f344ea18e067e111
Laimonas Milašius <laimis.milasius@gmail.com>
2025-09-14 18:15:18 +0300

    fix: cast open_time to bigint before converting

---
+++ migrations/1700000000007_add_open_time_dt.js
@@ -0,0 +1,14 @@
+export async function up(pgm) {
+  pgm.addColumn('signals', {
+    open_time_dt: {
+      type: 'timestamp',
+      notNull: true,
+      expressionGenerated:
+        "(CASE WHEN pg_typeof(open_time) = 'timestamp without time zone'::regtype THEN open_time ELSE to_timestamp((open_time)::bigint / 1000.0) END) AT TIME ZONE 'Europe/Vilnius'",
+    },
+  });
+}
+
+export async function down(pgm) {
+  pgm.dropColumn('signals', 'open_time_dt');
+}
